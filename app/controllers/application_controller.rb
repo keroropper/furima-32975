cd4a693162603c3ca8676f82279fd6b55d8ebf3d
keroropper <somato.0311@gmail.com>
2020-12-29 21:19:45 +0900

    新規登録、ログイン、ログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :birthday])
+    devise_parameter_sanitizer.permit(:sign_in,keys:[:email, :password])
   end
 end
